565cca141eeb72667c76e41df37b9d95dd2bb6e9
Trustt52 <whytrttttbam@mail.ru>
2025-09-13 15:06:11 +0300

    pervoe_zadanie

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun compressRuns(input: String): String {
+    if (input.isEmpty()) return ""
+    val sb = StringBuilder()
+    var current = input[0]
+    var count = 1
+    for (i in 1 until input.length){
+        val ch = input[i]
+        if (ch == current){ count++
+        }else{
+            sb.append(current)
+            if (count>1)
+                sb.append(count)
+            current = ch
+            count = 1
+        }
+    }
+    sb.append(current)
+    if (count>1) sb.append(count)
+    return sb.toString()
+} fun main(){
+    val input = "AAADSSSRRTTHAAAA"
+    val result = compressRuns(input)
+    println(result)
 }
